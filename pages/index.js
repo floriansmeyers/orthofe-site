4283e2439435eab8e7116f8f1eac6d485b2893e7
floriansmeyers <floriansmeyers@gmail.com>
2023-03-16 22:32:35 +0100

    change to orthofe

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,20 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>SFLOW BV</title>
+        <title>OrthoFe BV</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
 
-      <img style={{width: '90px'}} src='LOGO.png'></img>
-
-        <Header title="SFLOW BV" />
+        <Header title="OrthoFe BV" />
     
         <p className="description" style={{textAlign: 'center'}}>
-         BTW. nr. <span>BE0785.972.390</span><br/>
-         KBC <span>BE79 7340 5938 5833</span><br/><br/>
-          Polderstraat 37<br/>
-          2491 Balen-Olmen<br/><br/>
-          <a href="mailto:florian@sflow.be">florian@sflow.be</a><br/>
-          +32 473 49 20 81
+         BTW. nr. <span>BE0788.677.009</span><br/>
+         KBC <span>BE75 7370 6282 7751</span><br/><br/>
+          Dorp 41 bus 2<br/>
+          3221 Nieuwrode<br/><br/>
+          <a href="mailto:feline@orthofe.be">feline@orthofe.be</a><br/>
         </p>
       </main>
 
